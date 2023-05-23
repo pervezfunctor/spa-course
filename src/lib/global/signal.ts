fb6bac1f9291e156e1d9b6e8ab26b2261cf4530f
Pervez Iqbal <pervezfunctor@gmail.com>
2023-05-23 23:54:10 +0530

    additional signal functions

--- src/lib/global/signal.ts
+++ src/lib/global/signal.ts
@@ -5,12 +5,20 @@ import {
   type Getter,
   type PrimitiveAtom,
   type WritableAtom,
+  useAtomValue,
+  useSetAtom,
+  Atom,
 } from 'jotai'
 import { selectAtom } from 'jotai/utils'
 import type { Read, Write } from './types'
+import React from 'react'
 
 export function signal<Value extends object>(initialValue: Value) {
   return atom(initialValue)
+}
+
+export function snapshot<Value extends object>(signal: PrimitiveAtom<Value>) {
+  return () => useAtomValue(signal)
 }
 
 export function signals<Value>() {
@@ -19,6 +27,11 @@ export function signals<Value>() {
 
 export function computed<Value>(read: Read<Value>) {
   return atom(read)
+}
+
+export function computedHook<Value>(read: Read<Value>) {
+  const atom = computed(read)
+  return () => useAtomValue(atom)
 }
 
 export const select = selectAtom
@@ -30,7 +43,7 @@ export function derived<Value, Args extends unknown[], Result extends void>(
   return atom(read, write)
 }
 
-function action$<Value, Args extends unknown[], Result extends void>(
+export function action<Value, Args extends unknown[], Result extends void>(
   signal: WritableAtom<Value, any, Result>,
   fn: (get: Getter, draft: Draft<Value>, ...args: Args) => void,
 ) {
@@ -43,11 +56,32 @@ function action$<Value, Args extends unknown[], Result extends void>(
   })
 }
 
-export function action<Value, Args extends unknown[], Result extends void>(
+export function actionHook<Value, Args extends unknown[], Result extends void>(
   signal: WritableAtom<Value, any, Result>,
   fn: (get: Getter, draft: Draft<Value>, ...args: Args) => void,
 ) {
-  const actionAtom = action$(signal, fn)
+  const actionAtom = action(signal, fn)
 
   return () => useAtom(actionAtom)
+}
+
+export function useValue<Value>(
+  signal: Atom<Value> | Atom<Promise<Value>> | Atom<Value | Promise<Value>>,
+) {
+  return useAtomValue(signal)
+}
+
+export const useAction = useSetAtom
+
+export function useSignal<
+  Value,
+  Args extends unknown[],
+  Result extends void | Promise<void>,
+>(atom: WritableAtom<Value, Args, Result>) {
+  return [useValue(atom), useAction(atom)]
+}
+
+export function useLocalSignal<Value extends object>(initialValue: Value) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return useSignal(React.useMemo(() => signal(initialValue), []))
 }
