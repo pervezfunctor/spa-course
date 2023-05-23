5e99096feb5b76dd77130cc34341b8383d88de28
Pervez Iqbal <pervezfunctor@gmail.com>
2023-05-23 13:05:06 +0530

    msw support

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './index.css'
 
+if (import.meta.env.DEV) {
+  const { worker } = await import('./mocks/browser')
+  worker.start({ onUnhandledRequest: 'bypass' })
+}
+
 const root = createRoot(document.getElementById('root')!)
 
 root.render(
